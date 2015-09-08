842c5581eb5386cff564078ed94638b5d016327c
way0750 <bruce0750@gmail.com>
2015-09-07 18:19:22 -0700

    getElementByClassName works now

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+// 
+
+var hasClassValueOf = function(node, classValue){
+	//we are only checking element type of node; if nodeType return 1 then it is an element type node
+	return node.nodeType === 1 ? node.className.search(classValue) > -1 : false;
 };
+
+var getElementsByClassName = function(className, nodes
+){
+  nodes = nodes || document.childNodes;
+
+  return _.reduce(nodes, function(passedNodes, node){
+  	if (node.nodeType !== 1) { return passedNodes;}
+  	if (hasClassValueOf(node, className)) {passedNodes.push(node);}
+  	var children = node.childNodes;
+  	
+  	if (children.length){
+  		passedNodes = passedNodes.concat(getElementsByClassName(className, children));
+  	}
+  	return passedNodes;
+
+  }, []);
+
+};
+
